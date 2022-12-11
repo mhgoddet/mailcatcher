8c9651bbfe987a50f06666092d6cf813a28ab1f5
mhgoddet <mariehelenegoddet+github@gmail.com>
2022-12-11 14:10:05 +0100

    test

--- spec/command_spec.rb
+++ spec/command_spec.rb
@@ -11,6 +11,7 @@ RSpec.describe "mailcatcher command" do
 
   context "--help" do
     it "shows help then exits" do
+
       expect { system %(mailcatcher --help) }
         .to output(a_string_including("MailCatcher v#{MailCatcher::VERSION}") & a_string_including("--help") & a_string_including("Display this help information"))
         .to_stdout_from_any_process
